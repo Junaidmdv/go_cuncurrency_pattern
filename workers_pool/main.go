3f812b2daa538c75aaa9af1d2840eff9128f3acd
Junaidmdv <juni313786@gmail.com>
2025-05-04 13:52:08 +0530

    Add practiced notes

--- workers_pool/main.go
+++ workers_pool/main.go
@@ -5,71 +5,55 @@ import (
 	"sync"
 )
 
+func worker(wg *sync.WaitGroup, jobchan chan int, reschan chan int) {
+	defer wg.Done()
 
-type job struct{
-	Id  int
-	Value int
+	for job := range jobchan {
+		reschan <- job * job
+	}
 }
 
-type Result struct{
-	Id int 
-	Square int
-}
+func FinalResult(wg *sync.WaitGroup, reschan chan int) {
 
-func worker(w int,jobs <-chan job,result chan<- Result,wg *sync.WaitGroup){
-    defer wg.Done()
-    for job:=range jobs{
-       result <-Result{Id: job.Id,Square: job.Value*job.Value}
+	defer wg.Done()
+
+	for results := range reschan {
+		fmt.Println(results)
 	}
 
 }
-func FinalResults(resWg *sync.WaitGroup, results <-chan Result){
-    defer resWg.Done()
 
-	for result:=range results{
-		 fmt.Println(result)
-	}
+func processJob(workers, jobs int) {
 
+	jobChan := make(chan int, jobs)
+	resChan := make(chan int, jobs)
 
-}
-
-
-
-
-func dispacture(jobcount int ,workers_count int){
-	jobs:=make(chan job,jobcount)
-	results:=make(chan Result,jobcount)
-	 
 	var wg sync.WaitGroup
 
-	wg.Add(workers_count)
-
-	for w:=1;w<=workers_count;w++{
-		 go worker(w,jobs,results,&wg)
+	wg.Add(workers)
+	for i := 1; i <= workers; i++ {
+		go worker(&wg, jobChan, resChan)
 	}
-    
+
+	for i := 1; i <= jobs; i++ {
+		jobChan <- i
+	}
+	close(jobChan)
+
 	var resWg sync.WaitGroup
 	resWg.Add(1)
+	go FinalResult(&resWg, resChan)
 
-	go FinalResults(&resWg,results)
-
-	for i:=1;i<=jobcount;i++{
-		jobs <- job{Id: i,Value: i}
-	}
-	close(jobs)
 	wg.Wait()
-	close(results)
+	close(resChan)
 	resWg.Wait()
 }
 
-func main(){
-	const (
-		workers_count=10
-		jobs_count=100
+func main() {
 
-	)
+	workers := 5
+	jobs := 100
 
-	fmt.Println("starting batch collection processing along with syncronisation")
+	processJob(workers, jobs)
 
-	dispacture(jobs_count,workers_count)
 }
